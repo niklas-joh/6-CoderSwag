03a8b815979dbe517b3863bc42f56c0d61fc9683
Niklas Johansson <niklas@hjuvik.net>
2019-04-28 10:13:41 +0200

    Initial commit

---
+++ coder-swag/coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Niklas Johansson on 28-04-2019.
+//  Copyright © 2019 Anaconda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
