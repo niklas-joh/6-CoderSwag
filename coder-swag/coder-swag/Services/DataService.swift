5c03b4fdd04fda2ad9dc685e90c7f59940f95435
Niklas Johansson <niklas@hjuvik.net>
2019-05-30 11:42:34 +0200

    feat: Create Table Views on main screen
    This adds table views with product categories for the app on the start page.

---
+++ coder-swag/coder-swag/Services/DataService.swift
@@ -0,0 +1,23 @@
+//
+//  DataService.swift
+//  coder-swag
+//
+//  Created by Niklas Johansson on 19-05-2019.
+//  Copyright © 2019 Anaconda. All rights reserved.
+//
+
+import Foundation
+class DataService {
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    func getCategories() -> [Category] {
+        return categories
+    }
+}
